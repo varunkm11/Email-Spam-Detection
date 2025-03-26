b4baf0b0c35bd07a9036dd6a8dc7c5518f50c8b2
varunkm11 <varunkumarsingh818@gmail.com>
2025-03-26 10:20:15 +0530

    Day 2 of Email Spam Detection Project

--- SpamDetection.py
+++ SpamDetection.py
@@ -1,5 +1,17 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB                       
 
 data = pd.read_csv("D:\CV Projects\Email-Spam-Detection\spam.csv")
-print(data.shape)
-data.drop_duplicate(inplace=True)
+
+data.drop_duplicates(inplace=True)
+data['Category'] = data['Category'].replace(['ham','spam'],['Not Spam' ,'Spam'])
+
+mess = data['Message']
+cat = data['Category']
+(mess_train,mess_test,cat_train,cat_test) = train_test_split(mess ,cat ,test_size=0.2)
+cv = CountVectorizer(stop_words='english')
+cv.fit_transform(mess_train)
+
+#creating model
