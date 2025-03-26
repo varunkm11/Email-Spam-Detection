7043f250d2cd9de5532bf161b1db013bab3203ea
varunkm11 <varunkumarsingh818@gmail.com>
2025-03-26 19:26:49 +0530

    Day 2 second Commit

--- SpamDetection.py
+++ SpamDetection.py
@@ -13,5 +13,13 @@ cat = data['Category']
 (mess_train,mess_test,cat_train,cat_test) = train_test_split(mess ,cat ,test_size=0.2)
 cv = CountVectorizer(stop_words='english')
 cv.fit_transform(mess_train)
+featuers = cv.fit_transform(mess_train)
 
 #creating model
+
+model = MultinomialNB()
+model.fit(featuers, cat_train)
+
+#test our model
+featuers_test = cv.transform(mess_test)
+print(model.score(featuers_test, cat_test))
